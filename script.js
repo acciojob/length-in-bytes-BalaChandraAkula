8a2dda416c7e6b98cea6a322f6f3f3a2d534ec03
Bala Chandra Akula <115984987+BalaChandraAkula@users.noreply.github.com>
2023-05-09 12:02:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const byteSize = (str) => {
   // write your code here
+	const encoder = new TextEncoder();
+  const encodedArray = encoder.encode(str);
+  
+  // Return the length of the array
+  return encodedArray.length;
 };
 
 // Do not change the code below
